9baa0ec031fbc3e56b0841796cd0ec6b154bb125
Alessio <108763332+alessioVelluso@users.noreply.github.com>
2024-01-15 23:00:50 +0100

    Update _update_models.sh

--- models/_update_models.sh
+++ models/_update_models.sh
@@ -1,5 +1,5 @@
 # GET .ENV VARIABLES
-export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' .env | tr -d '\r' | xargs)
 
 # UPDATE-SCRIPT
 node_modules\\.bin\\sequelize-auto -h $DB_HOST -d $DB_NAME -u $DB_USER -x $DB_PASSWORD -p $DB_PORT --dialect $DB_DIALECT -o ./models -l ts
